278404efc44a90fc2f35f78362200c10542ac0af
Frags1187 <raymondleeyl123@gmail.com>
2021-11-25 16:55:26 +0800

    UI done

---
+++ components/Charts/VerticalBarChart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            backgroundColor: '#615055',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            backgroundColor: '#946E83',
+        },
+    ],
+};
+
+export default function VerticalBarChart() {
+    return <Bar data={data} />;
+}
